1841eee31b42d283d212492f53f453c41ad8832f
Guo Jiaming <176566340@qq.com>
2013-12-30 09:52:51 +0800

    测试提交

--- app.js
+++ app.js
@@ -65,5 +65,5 @@ app.post('/post', function(req, res, next) {
 });
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port') + 'test');
 });
